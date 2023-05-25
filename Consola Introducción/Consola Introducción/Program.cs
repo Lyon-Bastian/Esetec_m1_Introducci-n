e19ac48a282ade3f805b4c00b1308703e684b0d2
Alejandro <shion_vivias@hotmail.com>
2023-05-25 20:58:43 +0100

    Introducción al C#

---
+++ Consola Introducción/Consola Introducción/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Consola_Introducción
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Pulsa una tecla");
+            Console.ReadKey();
+        }
+    }
+}

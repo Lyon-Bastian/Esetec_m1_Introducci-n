73acbbd99bd7fb42285f7924c525cbe944959708
Alejandro <shion_vivias@hotmail.com>
2023-06-02 19:40:25 +0100

    Nuevo dia, mas códigos.

---
+++ Clase 2 de Junio/Clase 2 de Junio/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_2_de_Junio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//Creando un codico con un "array" o matriz
+            int numeroAlumnos = 10;
+            int[] notas = new int[10];
+            int suma = 0;
+            notas[0] = 4;
+            notas[1] = 5;
+            notas[2] = 6;
+            notas[3] = 8;
+            notas[4] = 7;
+            notas[5] = 9;
+
+            //Console.WriteLine(notas[0]);
+
+            for (int i = 0; i < numeroAlumnos; i++) //<---- los "for" se usan como intervalos
+            {
+                //Console.WriteLine(notas[i]);
+                suma= suma + notas[i];
+                
+            }
+            for (int i = 0;i < numeroAlumnos;i++) 
+            {
+                Console.WriteLine(notas[i]);
+            }
+
+            Console.WriteLine(suma);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Clase dia 2 de junio-2/Clase dia 2 de junio-2/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_dia_2_de_junio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroAlumnos = 10;
+            
+            GenerarNotas(numeroAlumnos);
+            SumarNotas(numeroAlumnos);
+            MayorNota(numeroAlumnos);
+            Console.WriteLine("Pulsa cualquier tecla para salir");
+            Console.ReadKey();
+        }
+
+        //Metodo que nos dice cual es la mayor notaque se ha puesto
+        //generando n notas aleatorias
+        static public void MayorNota(int numeroAlumnosInterno)
+        {
+            int[] notas = new int[numeroAlumnosInterno];
+            int numeroMayor = 0;
+
+            Random rand = new Random();
+
+            //Generar notas aleatoriamente y elegir el mayor
+            for (int i = 0; i < numeroAlumnosInterno; i++)
+            {
+                notas[i] = rand.Next(10);
+                if (numeroMayor < notas[i])
+                    numeroMayor = notas[i];
+                Console.Write(notas[i] + " ");
+            }
+            Console.WriteLine("La nota mas alta es: " + numeroMayor);
+
+            //cuantas personas han sacado la mayor nota
+            int cantidadDeNotasMayores = 0;
+                for(int i = 0;i < numeroAlumnosInterno; i++)
+                {
+                    if (notas[i] == numeroMayor)
+                    {
+                        cantidadDeNotasMayores++;
+                    }
+                    
+                }
+
+            Console.WriteLine("La máxima nota ha aparecido " + cantidadDeNotasMayores + " veces");
+        }
+
+            
+        static public void SumarNotas(int numeroAlumnosInterno)
+        {
+            int[] notas = new int[numeroAlumnosInterno];
+            int[] notas2 = new int[numeroAlumnosInterno];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < numeroAlumnosInterno; i++)
+            {
+                notas[i] = rand.Next(10);
+                notas2[i] = rand.Next(10); //genera un número aleatorio
+            }
+        }
+        static public void GenerarNotas(int numeroAlumnosInterno)
+        {
+            int[] notas = new int[numeroAlumnosInterno];
+            int suma = 0;
+            int media = 0;
+
+            Random random = new Random();
+           // int valor = random.Next(10);
+            
+            //Generar las notas aleatoriamente, imprimirlas y sumarlas
+            for (int i = 0; i < numeroAlumnosInterno; i++) 
+            {
+                notas[i] = random.Next(10);
+                Console.WriteLine(notas[i]);
+                suma = suma + notas[i];
+                media = suma / numeroAlumnosInterno;
+
+            }
+            //Suma todas las notas
+            /*for (int i = 0;i < numeroAlumnosInterno;i++) 
+            {
+               suma = suma + notas[i];
+            }*/
+            Console.WriteLine("Suma de todas las notas: " + suma);
+            Console.WriteLine("Nota Media: " + media);
+        }
+
+    }
+}
